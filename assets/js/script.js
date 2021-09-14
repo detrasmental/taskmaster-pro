1009c243ef9f416bbe2b472a39120c80e95e6eb1
detrasmental <detra.gordon76@gmail.com>
2021-09-14 16:36:42 -0400

    added Bootstrap utility

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,50 +84,47 @@ $("#task-form-modal .btn-primary").click(function() {
 $(".list-group").on("click", "p", function() {
   var text = $(this)
     .text()
-     .trim();
-  console.log(text);
-  $(".list-group").on("blur", "textarea", function() {
-    // get the textarea's current value/text
+    .trim();
+ 
+//replace p element with new text area
+var textInput = $("<textarea>")
+.addClass("form-control")
+.val(text);
+$(this).replaceWith(textInput);
+
+textInput.trigger("focus");
+});
+
+// field unfocused
+$(".list-group").on("blur", "textarea", function() {
+
+    
+// get the textarea's current value/text
 var text = $(this)
-.val()
-.trim();
+  .val();
+  
 
 // get the parent ul's id attribute
 var status = $(this)
-.closest(".list-group")
-.attr("id")
-.replace("list-", "");
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
 
 // get the task's position in the list of other li elements
 var index = $(this)
-.closest(".list-group-item")
-.index();
+  .closest(".list-group-item")
+  .index();
 
 tasks[status][index].text = text;
 saveTasks();
-});
-
-  var textInput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
 
 // recreate p element
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(text);
-// replace textarea with p element
-    $(this).replaceWith(taskP); 
-});
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
 
-// remove all tasks
-$("#remove-tasks").on("click", function() {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
-  }
-  saveTasks();
+// replace textarea with p element
+$(this).replaceWith(taskP);
 });
 
 // due date was clicked
@@ -180,7 +177,15 @@ $(".list-group").on("blur", "input[type='text']", function() {
   // replace input with span element
   $(this).replaceWith(taskSpan);
 });
+
+// remove all tasks
+$("#remove-tasks").on("click", function() {
+  for (var key in tasks) {
+    tasks[key].length = 0;
+    $("#list-" + key).empty();
+  }
+  saveTasks();
+});
+
 // load tasks for the first time
 loadTasks();
-
-
