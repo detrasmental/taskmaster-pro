a9d2f18ecebfcb7449a3ccb18fa786148f63492e
detrasmental <detra.gordon76@gmail.com>
2021-09-21 11:06:35 -0400

    restarted to fix error

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,49 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+
+var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+var text = $(this)
+.val()
+.trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+.closest(".list-group")
+.attr("id")
+.replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+.closest(".list-group-item")
+.index();
+
+tasks[status][index].text = text;
+saveTasks();
+
+// recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
+
+});
+
 
 
 // modal was triggered
@@ -79,52 +122,6 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
-});
-
-$(".list-group").on("click", "p", function() {
-  var text = $(this)
-    .text()
-    .trim();
- 
-//replace p element with new text area
-var textInput = $("<textarea>")
-.addClass("form-control")
-.val(text);
-$(this).replaceWith(textInput);
-
-textInput.trigger("focus");
-});
-
-// field unfocused
-$(".list-group").on("blur", "textarea", function() {
-
-    
-// get the textarea's current value/text
-var text = $(this)
-  .val();
-  
-
-// get the parent ul's id attribute
-var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
-
-// get the task's position in the list of other li elements
-var index = $(this)
-  .closest(".list-group-item")
-  .index();
-
-tasks[status][index].text = text;
-saveTasks();
-
-// recreate p element
-var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-
-// replace textarea with p element
-$(this).replaceWith(taskP);
 });
 
 // due date was clicked
@@ -177,7 +174,6 @@ $(".list-group").on("blur", "input[type='text']", function() {
   // replace input with span element
   $(this).replaceWith(taskSpan);
 });
-
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -189,3 +185,8 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+
+
+
+
